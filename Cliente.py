631f187c6afd0a51b95892d0b96f35ade17488cd
Guilherme <76690766+guilherme3987@users.noreply.github.com>
2024-04-01 21:28:13 -0300

    Add files via upload

--- Cliente.py
+++ Cliente.py
@@ -5,34 +5,39 @@ red = "\033[91m"
 green = "\033[92m"
 yellow = "\033[93m"
 blue = "\033[94m"
-reset = "\033[0m" 
-
+reset = "\033[0m"
 
 def hora():
     hora_atual = datetime.datetime.now()
-    hora_formatada = hora_atual.strftime(blue+"%H:%M:%S")
+    hora_formatada = hora_atual.strftime(blue + "%H:%M:%S")
     return hora_formatada
 
-# Ip e porta do socket
+def cliente():
+    # Ip e porta do socket
+    ip = '127.0.0.1'
+    porta = 3322
 
-ip = '127.0.0.1'
-porta = 3322
+    cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    cliente.connect((ip, porta))
 
-cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-cliente.connect((ip, porta))
+    while True:
+        try:
+            # Começar conversa
+            conversa = input(red + "=: ")
+            cliente.send(conversa.encode())
 
-while True:
-    # Começar conversa
-    conversa = input(red+"=: ")
-    cliente.send(conversa.encode())
+            # Verifica se o usuário deseja sair
+            if conversa.strip() == '0':
+                print(red + "Desconectado.")
+                cliente.close()
+                break
 
-    # Verifica se o usuário deseja sair
-    if conversa.strip() == '0':
-        print(red+"Desconectado.")
-        cliente.close()
-        break
+            # Recebe a resposta do servidor
+            resposta_conversa = cliente.recv(1024).decode()
+            print(blue, resposta_conversa, "  (", hora(), ")")
+        except ConnectionResetError:
+            print(red + "Conexão perdida.")
+            cliente.close()
+            break
 
-    # Recebe a resposta do servidor
-    resposta_conversa = cliente.recv(1024).decode()
-    print(blue,resposta_conversa,"  (",hora(),")")
-
+cliente()
--- Servidor.py
+++ Servidor.py
@@ -1,66 +1,74 @@
 import socket
+import threading
 import datetime
+
+#Definição de cores para a formatação do texto:
 
 red = "\033[91m"
 green = "\033[92m"
 yellow = "\033[93m"
 blue = "\033[94m"
-reset = "\033[0m" 
+reset = "\033[0m"
 
-
+#Função menu
 def menu():
-    print(red+"--- Menu --- \n\nEntrar no chat -- 1\nSair no chat -- 0")
+    print(red + "--- Menu --- \n\nEntrar no chat -- 1\nSair no chat -- 0")
 
 menu()
 
+#Função que retorna hora
 def hora():
     hora_atual = datetime.datetime.now()
-    hora_formatada = hora_atual.strftime(blue+"%H:%M:%S")
+    hora_formatada = hora_atual.strftime(blue + "%H:%M:%S")
     return hora_formatada
 
+# Função para lidar com a conexão com o cliente
+def lidando_cliente(cliente, address):
+    while True:
+        try:
+            # Recebe a mensagem do cliente
+            msg = cliente.recv(1024).decode()
+
+            # Sair do chat
+            if msg.strip() == '0':
+                print(red, address, "Desconectou.")
+                cliente.close()
+                break
+
+            print(msg, address, "  (", hora(), ")")
+
+            # Responde ao cliente
+            conversa = input("=: ")
+            cliente.send(conversa.encode())
+        except ConnectionResetError:
+            print(red, address, "Desconectou.")
+            cliente.close()
+            break
+
+# Configuração do servidor
 def servidor():
-    escolha = int(input(red+"->"))
+    escolha = int(input(red + "->"))
 
-    if escolha == 1 :
-
-        # Definindo porta e ip para conexão
+    if escolha == 1:
+        # Definindo porta,ip e socket
         ip = '127.0.0.1'
         porta = 3322
 
         servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         servidor.bind((ip, porta))
-        servidor.listen( )
+        servidor.listen()
 
-        print(green,"Esperando conexão...")
+        print(green, "Esperando conexão...")
 
+        # Aceita a conexão do cliente
         while True:
-            # Aceita a conexão do cliente
+            
             cliente, address = servidor.accept()
-            print(green,"Conexão estabelecida com ",address,hora())
+            print(green, "Conexão estabelecida com ", address, hora())
 
-            while True:
-                # Recebe a mensagem do cliente
-                msg = cliente.recv(1024).decode()
-
-                # Sair do chat
-                if msg.strip() == '0':
-                    print(red,address,"Desconectou.")
-                    cliente.close()
-                    break
-
-                print(msg,address,"  (",hora(),")")
-
-                # Responde ao cliente
-                conversa = input("=: ")
-                cliente.send(conversa.encode())
-
-
-    else: print(red,"Fim")
-
-
+            #Lidar com várias conexões
+            threading.Thread(target=lidando_cliente, args=(cliente, address)).start()
+    else:
+        print(red, "Fim")
 
 servidor()
-
-
-
-
